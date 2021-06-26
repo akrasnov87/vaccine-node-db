3828fc636df7d5289e1acd5c5a57750d3171e0da
Александр Краснов <akrasnov87@gmail.com>
2021-06-26 14:03:11 +0300

    update

--- router/upload.js
+++ router/upload.js
@@ -13,9 +13,10 @@ module.exports = function () {
     router.get("/file", function (req, res) {
         var id = req.query.id;
         var type = req.query.type;
-        var filters = [{ property: "id", operator: "=", value: id }];
+        var filters = [{ property: "f_document", operator: "=", value: id }, { property: 'ba_' + type, operator: "isnot", value: null }];
+        var sort = [{ property: "dx_created", direction: "DESC" }];
 
-        db.provider.select('core', 'dd_files', { limit: 1, select: 'id, ba_' + type, filter: filters }, null, function (data) {
+        db.provider.select('core', 'dd_files', { limit: 1, select: 'id, ba_' + type, filter: filters, sort: sort }, null, function (data) {
             if (data.meta.success && data.result.records.length > 0) {
                 res.setHeader('Content-Disposition', 'attachment; filename=' + id + '.' + type);
                 res.setHeader("Content-Type", type == "jpg" ? 'image/jpeg' : 'application/pdf');
@@ -30,7 +31,7 @@ module.exports = function () {
         var id = req.query.id;
         var files = req.files;
 
-        var obj = { id: id }
+        var obj = { f_document: id }
         if (files["jpg"]) {
             obj["ba_jpg"] = files["jpg"].data
         }
