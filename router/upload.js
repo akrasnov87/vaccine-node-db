83391509a4e88b090e8cc77bd0faf5d349222379
Александр Краснов <akrasnov87@gmail.com>
2021-06-28 19:47:01 +0300

    обновление

--- router/upload.js
+++ router/upload.js
@@ -13,16 +13,16 @@ module.exports = function () {
     router.get("/file", function (req, res) {
         var id = req.query.id;
         var type = req.query.type;
-        var filters = [{ property: "f_document", operator: "=", value: id }, { property: 'ba_' + type, operator: "isnot", value: null }];
+        var filters = [{ property: "f_document", operator: "=", value: id }, { property: 'c_type', operator: "=", value: type }];
         var sort = [{ property: "dx_created", direction: "DESC" }];
 
-        db.provider.select('core', 'dd_files', { limit: 1, select: 'id, ba_' + type, filter: filters, sort: sort }, null, function (data) {
+        db.provider.select('core', 'dd_files', { limit: 1, select: 'id, ba_data', filter: filters, sort: sort }, null, function (data) {
             if (data.meta.success && data.result.records.length > 0) {
-                if (type == 'jpg') {
+                if (type == 'sert') {
                     res.setHeader('Content-Disposition', 'attachment; filename=' + id + '.' + type);
                 }
-                res.setHeader("Content-Type", type == "jpg" ? 'image/jpeg' : 'application/pdf');
-                res.send(data.result.records[0]["ba_" + type]);
+                res.setHeader("Content-Type", type != "sert" ? 'image/jpeg' : 'application/pdf');
+                res.send(data.result.records[0]["ba_data"]);
             } else {
                 res.send(data.meta.msg);
             }
@@ -31,16 +31,15 @@ module.exports = function () {
 
     router.get("/filebyid", function (req, res) {
         var id = req.query.id;
-        var type = req.query.type;
         var filters = [{ property: "id", operator: "=", value: id }];
 
-        db.provider.select('core', 'dd_files', { select: 'id, ba_' + type, filter: filters }, null, function (data) {
+        db.provider.select('core', 'dd_files', { select: 'id, ba_data', filter: filters }, null, function (data) {
             if (data.meta.success && data.result.records.length > 0) {
-                if (type == 'jpg') {
+                if (type == 'sert') {
                     res.setHeader('Content-Disposition', 'attachment; filename=' + id + '.' + type);
                 }
-                res.setHeader("Content-Type", type == "jpg" ? 'image/jpeg' : 'application/pdf');
-                res.send(data.result.records[0]["ba_" + type]);
+                res.setHeader("Content-Type", type != "sert" ? 'image/jpeg' : 'application/pdf');
+                res.send(data.result.records[0]["ba_data"]);
             } else {
                 res.send(data.meta.msg);
             }
@@ -61,15 +60,13 @@ module.exports = function () {
 
     router.post("/file", function (req, res) {
         var id = req.query.id;
-        var dx_created = req.query.dx_created;
+        var d_date = req.query.d_date;
+        var c_type = req.query.c_type;
         var files = req.files;
 
-        var obj = { f_document: id, dx_created: dx_created };
-        if (files["jpg"]) {
-            obj["ba_jpg"] = files["jpg"].data
-        }
-        if (files["pdf"]) {
-            obj["ba_pdf"] = files["pdf"].data
+        var obj = { f_document: id, d_date: d_date, c_type: c_type };
+        if (files["data"]) {
+            obj["ba_data"] = files["data"].data
         }
 
         db.provider.insertOrUpdate('core', 'dd_files', 'id', obj, function (data) {
